5b4416b67a8902546c34cd131cdf7c1e9a604eaa
Alexis Rhote <alexis.rhote@gmail.com>
2020-03-31 13:14:20 -0500

    Create Main class

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static final boolean BARREN = false;
+    public static final boolean FERTILE = true;
+
+
+
+    public static void main(String[] args) {
+        boolean[][] farm;
+
+    }
+
+    public boolean[][] prepFarm(){
+        
+    }
+}
+
+
