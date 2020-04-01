9c5beef987a3b906b9ea50ddd392c0308e533ca6
Alexis Rhote <alexis.rhote@gmail.com>
2020-04-01 16:30:08 -0500

    Restructure project

--- src/BoundingBox.java
+++ src/BoundingBox.java
@@ -1,5 +1,3 @@
-package com.company;
-
 public class BoundingBox {
     Coordinate lowerLeft;
     Coordinate upperRight;
--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,5 +1,3 @@
-package com.company;
-
 public class Coordinate {
     int x;
     int y;
